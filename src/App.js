93bcc60b938b3d09cb4777992b122dfb11bbed49
unknown <qmurrayconn@go.pasadena.edu>
2021-11-02 13:25:12 -0700

    First

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Test from "./components/Test";
+import "./PageBackground.css"
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page-background-color">
+      <div>
+        <Test></Test>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,8 @@
+import './Test.css';
+
+function Test(){
+    return <div className="photo-box">Profile Photo</div>
+}
+
+
+export default Test;
